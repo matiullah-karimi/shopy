be3631c109fd964a30efe30be4a6cb85c53b4afd
Matiullah Karimi <matiullah.karimi@gmail.com>
2022-02-02 16:49:31 +0400

    Add wishlist screen

---
+++ lib/screens/wishlist_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WishlistScreen extends StatelessWidget {
+  const WishlistScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('Wishlist'),
+    );
+  }
+}
