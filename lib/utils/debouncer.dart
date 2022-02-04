cf05b9305e5890b79e8cc92d1120e65b3d157563
Matiullah Karimi <matiullah.karimi@gmail.com>
2022-02-04 22:59:49 +0400

    Add debouncer util

---
+++ lib/utils/debouncer.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class Debouncer {
+  final int milliseconds;
+  Timer? _timer;
+
+  Debouncer({required this.milliseconds});
+
+  run(VoidCallback action) {
+    if (null != _timer) {
+      _timer!.cancel();
+    }
+
+    _timer = Timer(Duration(milliseconds: milliseconds), action);
+  }
+}
