edd073c7c48c7bd838a16dcbb518be36b985b86b
Matiullah Karimi <matiullah.karimi@gmail.com>
2022-02-05 15:13:47 +0400

    Check auth state before favorite and add to cart actions

--- lib/widgets/product/add_to_cart_icon_widget.dart
+++ lib/widgets/product/add_to_cart_icon_widget.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shopy/constants/styles.dart';
 import 'package:shopy/model/product.dart';
+import 'package:shopy/providers/auth_provider.dart';
 import 'package:shopy/providers/cart_provider.dart';
+import 'package:shopy/screens/login_screen.dart';
+import 'package:shopy/utils/navigator.dart';
 
 class AddToCartIconWidget extends ConsumerWidget {
   const AddToCartIconWidget({
@@ -17,6 +20,12 @@ class AddToCartIconWidget extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
     void addToCart() {
+      final authState = ref.watch(authStateProvider);
+
+      if (!authState.authenticated) {
+        return navigate(context, const LoginScreen());
+      }
+
       ref.read(cartProvider.notifier).add(product);
       const snackBar = SnackBar(
         content: Text('Product added to the cart'),
--- lib/widgets/product/favorite_icon_widget.dart
+++ lib/widgets/product/favorite_icon_widget.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shopy/constants/styles.dart';
 import 'package:shopy/model/product.dart';
+import 'package:shopy/providers/auth_provider.dart';
 import 'package:shopy/providers/wishlist_provider.dart';
+import 'package:shopy/screens/login_screen.dart';
+import 'package:shopy/utils/navigator.dart';
 
 class FavoriteIconWidget extends ConsumerWidget {
   const FavoriteIconWidget({Key? key, required this.product}) : super(key: key);
@@ -13,8 +16,13 @@ class FavoriteIconWidget extends ConsumerWidget {
   Widget build(BuildContext context, ref) {
     final wishlistNotifier = ref.watch(wishlistProvider.notifier);
     bool isFavorite = ref.watch(wishlistProvider).isFavorite(product);
+    final authState = ref.watch(authStateProvider);
 
     void toggleFavorite() {
+      if (!authState.authenticated) {
+        return navigate(context, const LoginScreen());
+      }
+
       wishlistNotifier.toggle(product);
     }
 
